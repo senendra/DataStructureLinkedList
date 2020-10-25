20b59a42bfc746fbc44c59e1f8fe277c3ee913eb
Senendra <senendra.007@gmail.com>
2020-10-25 12:22:35 +0530

    [Senendra] Add: Adding Simple LinkedList program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LinkedList
+{
+    class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                this.head = node;             
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else
+            {
+                Node temp = head;
+                while(temp != null)
+                {
+                    Console.WriteLine(" "+temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+            LinkedList list = new LinkedList();
+            list.Add(30);
+            list.Add(56);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}

67abfad22026f1daa8679ef5af2e4562e643bdd1
Senendra <senendra.007@gmail.com>
2020-10-29 00:16:04 +0530

    [Senendra] Add: Added Insert method to insert element in linked list

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -56,5 +56,33 @@ namespace LinkedList
                 temp.next = node;
             }
         }
+        public void Insert(int position, int data)
+        {
+            Node node = new Node(data);
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            else if (position == 1)
+            {
+                node.next = head;
+                head = node;
+            }
+            else
+            {
+                Node n = head, previous = head, after;
+                for (int pos = 1; pos < position; pos++)
+                {
+                    n = n.next;
+                    if (pos == position - 2)
+                    {
+                        previous = n;
+                    }
+                }
+                after = n;
+                previous.next = node;
+                node.next = after;
+            }
+        }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,8 +8,10 @@ namespace LinkedList
             Console.WriteLine("Welcome to LinkedList Program");
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Append(30);
             list.Append(70);
+            list.Display();
+            Console.WriteLine("On inserting 30 at 2nd position");
+            list.Insert(2, 30);
             list.Display();
         }
     }
